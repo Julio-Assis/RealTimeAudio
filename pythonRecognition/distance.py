3433c9abee8bddd5f42430d4e6ce835393c0ff3c
Cal <calniotrey@gmail.com>
2019-05-28 18:02:46 +0200

    Fixed a bit the bugs and made the code more readable

--- pythonRecognition/distance.py
+++ pythonRecognition/distance.py
@@ -1,17 +1,28 @@
 import numpy
 
 def distance(vector1, vector2):
+    """
+    Takes 2 numpy arrays of shape (x,) and returns the norm2 of the distance btween them.
+    """
     return numpy.linalg.norm(vector1 - vector2)
 
 def dynamicDistance(sample, input):
-    S = len(sample) + 2 #some infinity rows beneath
-    T = len(input)
-    D = numpy.full([S, T], numpy.inf)
-    D[2, 0] = 0
+    """
+    sample : list of numpy arrays of shape (x, 1)
+    input : list of numpy arrays of shape (x, 1). Same x, but not necessarely as much arrays.
+    Returns the dynamic distance between them.
+    Warning : dynamicDistance(a, b) !== dynamicDistance(b, a) usually !
+    """
+    sampleSize = len(sample) + 2 #2 rows of infinity beneath
+    inputSize = len(input)
+    matrix = numpy.full([sampleSize, inputSize], numpy.inf)
+    matrix[2, 0] = 0
     #using (0, 1, 2) model
-    for s in range(2, S): #we start at 2 because there is 2 rows of Inf beneath
-        for t in range(1, T): #no possibility to go just up
+    for s in range(2, sampleSize): #we start at 2 because there is 2 rows of Inf beneath
+        for t in range(1, inputSize): #no possibility to go just up so no need to start at 0
             d = distance(sample[s-2], input[t-1])
-            D[s, t] = d + min(D[s-2, t-1], D[s-1, t-1], D[s, t-1])
-    #print(D[-1])
-    return D[-1][-1]
+            matrix[s, t] = d + min(
+                matrix[s-2, t-1],
+                matrix[s-1, t-1],
+                matrix[s, t-1])
+    return matrix[-1][-1]
--- pythonRecognition/project.py
+++ pythonRecognition/project.py
@@ -25,23 +25,51 @@ RETAKE samples
 REWORK stille calculator (mean of 10 sec ?/several different frame noise1, noise2, ... ?)
 """
 
-CHUNK = 1024 #chunk size
+CHUNK_SIZE = 1024 #chunk size
 FORMAT = pyaudio.paInt16 #2byte per sample : +-32768
 CHANNELS = 2
-RATE = 44100 #sampling rate
-NFFT=2048 #FFT size
-NUMCEP=16 #number of coefficients
+SAMPLING_RATE = 44100 #sampling rate
+FFT_SIZE=2048 #fast fourier size size
+COEF_NUMBERS=16 #number of coefficients
 RECORD_SECONDS = 5
-WAVE_OUTPUT_FILENAME = "down.wav"
-ENERGY_BEGIN = 11 #to be changed from mic (it's a log scale)
-ENERGY_BEGIN_NUMBERS = 2
-ENERGY_BEGIN_BEFORE_NUMBERS = 5
-ENERGY_END = 9
-ENERGY_END_NUMBERS = 5
+ENERGY_THRESHOLD_BEGIN = 12 #to be changed from mic (it's a log scale)
+ENERGY_THRESHOLD_BEGIN_NUMBERS = 2 #numbers of frame where the energy must exceed the ENERGY_THRESHOLD_BEGIN
+ENERGY_THRESHOLD_BEFORE_BEGIN_NUMBERS = 5 #numbers of frame that are added just before the threshold_begin is exceeded
+ENERGY_THRESHOLD_END = 10
+ENERGY_THRESHOLD_END_NUMBERS = 5
 ACTIVATION_WAIT = 1 #number of seconds to wait after mic activation
-format = "%dh"%(CHUNK * CHANNELS) #h for short, so 16bits beacause we use paInt16
+format = "%dh"%(CHUNK_SIZE * CHANNELS) #h for short, so 16bits because we use paInt16
 
 class Vectors:
+    """
+    Class that represents the acoustic features vectors.
+    period :
+        It represents the duration in seconds between 2 times frames (ie 1/SAMPLING_RATE).
+        It doesn't influence the recognition, so it may be left at 1.
+    _basicVectors :
+        It is a list of numpy arrays.
+        These arrays are of shape (COEF_NUMBERS,).
+        There is one array for each time frame.
+    _accousticVectors :
+        It is a list of numpy arrays.
+        These arrays are of shape (3*COEF_NUMBERS,).
+        There is one array for each frame, except the first and last one.
+        The arrays include the COEF_NUMBERS mfcc coefficients (same as in _basicVectors).
+        The COEF_NUMBERS next are the first derivative (central) of the mfcc coefficients.
+        The last COEF_NUMBERS are the second derivative (central) of the mfcc coefficients.
+
+    getAcousticVecetors():
+        returns _acousticVectors
+
+    addBasicVector(v):
+        add v to _basicVectors.
+        If there are at least 3 arrays in _basicVectors, then add a new array to _acousticVectors
+
+    getLastAccousticVectors():
+        If there is at least an acoustic vector then returns (the last acoustic vector, True)
+        Else returns (None, False)
+    """
+
     def __init__(self, period=1):
         self._basicVectors = []
         self._acousticVectors = []
@@ -50,14 +78,18 @@ class Vectors:
     def getAcousticVectors(self):
         return self._acousticVectors
     
-    def addBasicVector(self, v): #central derivative => 1 frame late
-        # v = v - meanVector(v)
-        self._basicVectors.append(v)
-        if len(self._basicVectors) > 2:
-            dv  = [(v[i] - self._basicVectors[-3][i])/(2*self.period) for i in range(len(v))]
-            ddv = [(v[i] - 2*self._basicVectors[-2][i] + self._basicVectors[-3][i])/(self.period**2) for i in range(len(v))]
-            av = numpy.concatenate((v, dv, ddv))
-            self._acousticVectors.append(av)
+    def addBasicVector(self, basicVector): #central derivative => 1 frame late
+        """
+        Adds basicVector to the basic vectors and possibly its derivative to acoustic vectors.
+        basicVector must be the array returned by the mfcc.
+        """
+        basicVector = basicVector - numpy.mean(basicVector)
+        self._basicVectors.append(basicVector)
+        if len(self._basicVectors) > 2: #if there are at least 3 basic vectors we can calculate the derivative
+            dBasicVector  = (basicVector - self._basicVectors[-3])/(2*self.period) #first derivative
+            ddBasicVector = (basicVector - 2*self._basicVectors[-2] + self._basicVectors[-3])/(self.period**2) #second derivative
+            acousticBasicVector = numpy.concatenate((basicVector, dBasicVector, ddBasicVector))
+            self._acousticVectors.append(acousticBasicVector)
     
     def getLastAcousticVectors(self):
         if len(self._acousticVectors):
@@ -67,71 +99,108 @@ class Vectors:
     def __len__(self):
         return len(self._acousticVectors)
             
-def meanVector(v, ignoreFirst=True):
+def meanVector(v, ignoreFirst=False):
     return numpy.mean(v[ignoreFirst:])
 
-def extractVectorsFromFile(file_path):
+def extractVectorsFromFile(filePath):
+    """
+    filePath : The path to the file to read.
+    Returns a list which contains the acoustic vectors of the audio file.
+    """
     vectors = Vectors()
-    numbersAfterBegin = 0
-    wf = wave.open(file_path, 'rb')
+    wf = wave.open(filePath, 'rb')
     p = pyaudio.PyAudio()
-    data = wf.readframes(CHUNK)
+    data = wf.readframes(CHUNK_SIZE)
     while data != b'':
-        t = struct.unpack(format, data)
-        m = extractVector(t[::2])
-        vectors.addBasicVector(m)
-        data = wf.readframes(CHUNK)
+        samples = struct.unpack(format, data)
+        basicVector = extractVector(samples[::2]) #samples[::2] because the samples are interleaved
+        vectors.addBasicVector(basicVector)
+        data = wf.readframes(CHUNK_SIZE)
     p.terminate()
     wf.close()
     return vectors.getAcousticVectors()
 
-def extractVector(t):
-    return mfcc(numpy.array(t), samplerate=RATE, nfft=NFFT, numcep=NUMCEP, appendEnergy=False)[0]
+def extractVector(samples):
+    """
+    samples : the list of the audio samples, of length CHUNK_SIZE
+    Returns the COEF_NUMBERS first mfcc coefficients
+    """
+    return mfcc(numpy.array(samples), samplerate=SAMPLING_RATE, nfft=FFT_SIZE, numcep=COEF_NUMBERS, appendEnergy=False)[0]
 
-def getEnergy(t):
-    return mfcc(numpy.array(t), samplerate=RATE, nfft=NFFT, numcep=1, appendEnergy=True)[0][0]
+def getEnergy(samples):
+    """
+    samples : the list of the audio samples, of length CHUNK_SIZE
+    Returns the energy of samples
+    """
+    return mfcc(numpy.array(samples), samplerate=SAMPLING_RATE, nfft=FFT_SIZE, numcep=1, appendEnergy=True)[0][0]
 
 def listWavFiles():
-    f = []
+    """
+    Returns the list of the names of the .wav files that are in the same folder as this file.
+    """
+    filesNameList = []
     files = os.listdir(os.path.dirname(os.path.realpath(__file__)))
     for file in files:
-        if ".wav" in file:
-            f.append(file)
-    return f
+        if ".wav" in file: #if it has a .wav extension
+            filesNameList.append(file)
+    return filesNameList
 
 def getWords():
+    """
+    Returns a dictionnary d.
+    The keys of d are the name of the files (without extension, ie without .wav).
+    The values are lists containing the acoustic vectors of the related file.
+    """
     words = dict()
     files = listWavFiles()
-    for file in files:
-        words[file.split(".")[0]] = extractVectorsFromFile(file)
+    for fileName in files:
+        words[fileName.split(".")[0]] = extractVectorsFromFile(fileName)
     return words
 
 def compare(input, words):
-    m = "Error"
-    d_min = math.inf
+    """
+    input: The list of the acoustic vectors of the recorded audio.
+    words : A dictionnary (string, [acoustic vector]) for example generated by getWords()
+    Returns the word minimizing the dynamic distance between its acoustic vectors and the input
+    Prints (word, distance) for each word.
+    """
+    minimumWord = "Error"
+    minimumDistance = math.inf
     for word in words.keys():
-        d = distance.dynamicDistance(words[word], input)
-        print(word, d)
-        if d < d_min:
-            d_min = d
-            m = word
-    return m
+        dist = distance.dynamicDistance(words[word], input)
+        print(word, dist)
+        if dist < minimumDistance:
+            minimumDistance = dist
+            minimumWord = word
+    return minimumWord
 
 def test():
-    d = getWords()
+    """
+    Records an audio signal and try to recognize the words being spoken in real time.
+    Returns matrixes, vectors.
+        matrixes : dictionnary (string word, numpy.array matrix).
+            word : the string of the word. For example "up", "left", ...
+            matrix : the matrix generated by the 0,1,2 way
+        vectors : A Vectors object representing the recorded audio
+
+    Prints (word currently being recognized, distance from the model to the audio) for each time frame.
+    Prints what is printed by compare()
+    Prints the result of compare (not real time).
+    """
+    wordsDictionnary = getWords()
     p = pyaudio.PyAudio()
     matrixes = {}
-    for word in d.keys():
-        matrixes[word] = numpy.full((len(d[word]) + 2, 1), numpy.inf)
+    for word in wordsDictionnary.keys():
+        matrixes[word] = numpy.full((len(wordsDictionnary[word]) + 2, 1), numpy.inf)
         matrixes[word][2, 0] = 0
         
     
     stream = p.open(format=FORMAT,
                     channels=CHANNELS,
-                    rate=RATE,
+                    rate=SAMPLING_RATE,
                     input=True,
                     input_device_index=1,
-                    frames_per_buffer=CHUNK)
+                    frames_per_buffer=CHUNK_SIZE)
     
     vectors = Vectors()
     waitAfterActivation(stream)
@@ -140,44 +209,41 @@ def test():
     frames = []
     
     numbersAfterBegin = 0
-    old_min_value = numpy.inf
+    oldMinValue = numpy.inf
     wordsRecognized = []
-    f = True
+    energyThresholdAttained = False
     
-    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)): #number of chunks
-        data = stream.read(CHUNK)
+    for i in range(0, int(SAMPLING_RATE / CHUNK_SIZE * RECORD_SECONDS)): #number of chunks
+        data = stream.read(CHUNK_SIZE)
         frames.append(data)
-        t = struct.unpack(format, data) #unpacks the bytes in an array of size CHUNK * CHANNELS (interleaved !)
-        m = extractVector(t[::2])
-        # if numbersAfterBegin >= ENERGY_BEGIN_NUMBERS or m[0] >= ENERGY_BEGIN:
-        #     numbersAfterBegin += 1
-        vectors.addBasicVector(m)
-        m, e = vectors.getLastAcousticVectors()
-        if e:
-            # print("---")
-            current_min = "None"
-            current_min_value = numpy.inf
-            for word in d.keys():
-                x = len(d[word]) + 2
-                matrixes[word] = numpy.concatenate((matrixes[word], numpy.full((x, 1), numpy.inf)), axis=1)
-                for s in range(2, x):
-                    dis = distance.distance(d[word][s-2], m)
-                    if f:
-                        f = False
-                        print(word, dis)
-                    matrixes[word][s, -1] = dis + min(matrixes[word][s-2, -2], matrixes[word][s-1, -2], matrixes[word][s, -2])
-                if matrixes[word][-1, -1] < current_min_value:
-                    current_min_value = matrixes[word][-1, -1]
-                    current_min = word
-                # print(word, matrixes[word][-1, -1])
-            old_min_value = current_min_value
-            # matrixes[word][1, -1] = old_min_value
-            wordsRecognized.append(current_min)
-            print(current_min, current_min_value)
-        # elif m[0] < ENERGY_BEGIN:
-        #     numbersAfterBegin = 0
-        #     vectors = Vectors()
-    print(compare(vectors.getAcousticVectors(), d))
+        samples = struct.unpack(format, data) #unpacks the bytes in an array of size CHUNK_SIZE * CHANNELS (interleaved !)
+        basicVector = extractVector(samples[::2])
+        if energyThresholdAttained or getEnergy(samples[::2]) >= ENERGY_THRESHOLD_BEGIN:
+            energyThresholdAttained = True
+            vectors.addBasicVector(basicVector)
+            acousticVector, e = vectors.getLastAcousticVectors()
+            if e:
+                # print("---")
+                currentMin = "None"
+                currentMinValue = numpy.inf
+                for word in wordsDictionnary.keys():
+                    x = len(wordsDictionnary[word]) + 2
+                    matrixes[word] = numpy.concatenate((matrixes[word], numpy.full((x, 1), numpy.inf)), axis=1)
+                    for s in range(2, x):
+                        dis = distance.distance(wordsDictionnary[word][s-2], acousticVector)
+                        matrixes[word][s, -1] = dis + min(
+                            matrixes[word][s-2, -2],
+                            matrixes[word][s-1, -2],
+                            matrixes[word][s, -2])
+                    if matrixes[word][-1, -1] < currentMinValue:
+                        currentMinValue = matrixes[word][-1, -1]
+                        currentMin = word
+                    # print(word, matrixes[word][-1, -1])
+                oldMinValue = currentMinValue
+                # matrixes[word][1, -1] = oldMinValue
+                wordsRecognized.append(currentMin)
+                print(currentMin, currentMinValue)
+    print(compare(vectors.getAcousticVectors(), wordsDictionnary))
     if not numbersAfterBegin:
         print("Try again louder")
     
@@ -188,89 +254,109 @@ def test():
     p.terminate()
     return matrixes, vectors
 
-def record(file_name):
+def record(fileName):
+    """
+    Record audio and save it to a file.
+    There is an energy threshold.
+    fileName : The name of the audio file where the recorded audio will be saved. (ex: "up.wav")
+    """
     numbersAfterBegin = 0
     numbersAfterEnd = 0
             
     p = pyaudio.PyAudio()
     stream = p.open(format=FORMAT,
                     channels=CHANNELS,
-                    rate=RATE,
+                    rate=SAMPLING_RATE,
                     input=True,
                     input_device_index=1,
-                    frames_per_buffer=CHUNK)
+                    frames_per_buffer=CHUNK_SIZE)
     waitAfterActivation(stream)
     print("* recording")
     frames = []
-    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)): #number of chunks
-        data = stream.read(CHUNK)
-        t = struct.unpack(format, data)
-        m = extractVector(t[::2])
-        e = getEnergy(t[::2])
-        if numbersAfterEnd < ENERGY_END_NUMBERS:
-            if (numbersAfterBegin >= ENERGY_BEGIN_NUMBERS or e >= ENERGY_BEGIN):
-                if numbersAfterBegin == ENERGY_BEGIN_NUMBERS:
-                    a = max(0, len(frames) - ENERGY_BEGIN_NUMBERS - ENERGY_BEGIN_BEFORE_NUMBERS)
-                    frames = frames[a:]
-                if m[0] >= ENERGY_BEGIN:
+    for i in range(0, int(SAMPLING_RATE / CHUNK_SIZE * RECORD_SECONDS)): #number of chunks
+        data = stream.read(CHUNK_SIZE)
+        samples = struct.unpack(format, data)
+        energy = getEnergy(samples[::2])
+        if numbersAfterEnd < ENERGY_THRESHOLD_END_NUMBERS:
+            #if we didn't stop recording
+            if (numbersAfterBegin >= ENERGY_THRESHOLD_BEGIN_NUMBERS or energy >= ENERGY_THRESHOLD_BEGIN):
+                #if we started recording
+                if numbersAfterBegin == ENERGY_THRESHOLD_BEGIN_NUMBERS:
+                    #if we just successfully passed the threshold enough times
+                    #we need to record the future, but also the ENERGY_THRESHOLD_BEGIN_NUMBERS frames before
+                    framesBegin = max(0, len(frames) - ENERGY_THRESHOLD_BEGIN_NUMBERS - ENERGY_THRESHOLD_BEFORE_BEGIN_NUMBERS)
+                    frames = frames[framesBegin:] #we start the recording with the ENERGY_THRESHOLD_BEGIN_NUMBERS last frames
+                if energy >= ENERGY_THRESHOLD_BEGIN:
+                    #if we pass the threshold, we won't stop recording for ENERGY_THRESHOLD_END_NUMBERS frames
                     numbersAfterBegin += 1
                     numbersAfterEnd = 0
-                else: #numbersAfterBegin >= ENERGY_BEGIN_NUMBERS
+                else: #numbersAfterBegin >= ENERGY_THRESHOLD_BEGIN_NUMBERS
+                    #if we don't pass the threshold, we need to know if we didn't pass it since a long time
                     numbersAfterEnd += 1
                 frames.append(data)
-            elif (numbersAfterBegin < ENERGY_BEGIN_NUMBERS and e < ENERGY_BEGIN):
+            elif (numbersAfterBegin < ENERGY_THRESHOLD_BEGIN_NUMBERS and energy < ENERGY_THRESHOLD_BEGIN):
+                #if we didn't start recording and the energy isn't high enough, we restart the counter
                 numbersAfterBegin = 0
     print("* done recording")
     stream.stop_stream()
     stream.close()
     p.terminate()
-    wf = wave.open(file_name, 'wb')
+    wf = wave.open(fileName, 'wb')
     wf.setnchannels(CHANNELS)
     wf.setsampwidth(p.get_sample_size(FORMAT))
-    wf.setframerate(RATE)
+    wf.setframerate(SAMPLING_RATE)
     wf.writeframes(b''.join(frames))
     wf.close()
 
 
-def recordNoise(file_name, duration=RECORD_SECONDS): #should probably be pretty small, will try this next time
+def recordNoise(fileName, duration=RECORD_SECONDS): #should probably be pretty small, will try this next time
+    """
+    Records audio and save it to a file.
+    Doesn't use energy threshold => use it for noise recording.
+    fileName : The name of the audio file where the recorded audio will be saved. (ex: "noise.wav")
+    duration : The duration of the recording.
+    """
     p = pyaudio.PyAudio()
     stream = p.open(format=FORMAT,
                     channels=CHANNELS,
-                    rate=RATE,
+                    rate=SAMPLING_RATE,
                     input=True,
                     input_device_index=1,
-                    frames_per_buffer=CHUNK)
+                    frames_per_buffer=CHUNK_SIZE)
     waitAfterActivation(stream)
     print("* recording")
     frames = []
-    for i in range(0, int(RATE / CHUNK * duration)): #number of chunks
-        data = stream.read(CHUNK)
+    for i in range(0, int(SAMPLING_RATE / CHUNK_SIZE * duration)): #number of chunks
+        data = stream.read(CHUNK_SIZE)
         frames.append(data)
     print("* done recording")
     stream.stop_stream()
     stream.close()
     p.terminate()
-    wf = wave.open(file_name, 'wb')
+    wf = wave.open(fileName, 'wb')
     wf.setnchannels(CHANNELS)
     wf.setsampwidth(p.get_sample_size(FORMAT))
-    wf.setframerate(RATE)
+    wf.setframerate(SAMPLING_RATE)
     wf.writeframes(b''.join(frames))
     wf.close()
 
 def energyPlot():
+    """
+    Plots the energy of the different frames.
+    """
     p = pyaudio.PyAudio()
     stream = p.open(format=FORMAT,
                     channels=CHANNELS,
-                    rate=RATE,
+                    rate=SAMPLING_RATE,
                     input=True,
                     input_device_index=1,
-                    frames_per_buffer=CHUNK)
+                    frames_per_buffer=CHUNK_SIZE)
     e = []
     waitAfterActivation(stream)
     print("* recording")
     frames = []
-    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)): #number of chunks
-        data = stream.read(CHUNK)
+    for i in range(0, int(SAMPLING_RATE / CHUNK_SIZE * RECORD_SECONDS)): #number of chunks
+        data = stream.read(CHUNK_SIZE)
         t = struct.unpack(format, data)
         m = getEnergy(t[::2])
         e.append(m)
@@ -282,7 +368,11 @@ def energyPlot():
     pylab.show()
 
 def waitAfterActivation(stream):
+    """
+    Utility function that is used to allow the mics to get online.
+    stream : The stream to read from.
+    """
     print("Recording in", ACTIVATION_WAIT)
     t = time.time()
     while (time.time() - t) < ACTIVATION_WAIT:
-        stream.read(CHUNK)
+        stream.read(CHUNK_SIZE)
