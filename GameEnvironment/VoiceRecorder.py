e11df0cba59b684f8cf1b5da1e7bbda1da981721
Julio Assis <juliosevarollia@gmail.com>
2019-06-14 14:24:05 +0200

    implemented VoiceRecorder

---
+++ GameEnvironment/VoiceRecorder.py
@@ -0,0 +1,35 @@
+import sounddevice as sd
+import numpy as np
+
+class VoiceRecorder:
+
+  GamesToCommands = {
+    'MsPacman-v0' : ['UP', 'DOWN', 'LEFT', 'RIGHT']
+  }
+
+  SavePath = './GameEnvironment/CommandRecords/'
+
+  def __init__(self, game_name):
+    self.game_name = game_name
+
+  def record_commands(self):
+    commands = VoiceRecorder.GamesToCommands[self.game_name]
+    for command in commands:
+      self.record_n_times(command)
+
+  def record_n_times(self, command, times=1, fs = 44100, duration=3):
+    
+    print('-'*20)
+    print("Let's record the command={} {} times".format(command, times))
+    for i in range(times):
+      print('Starting record {}'.format(i + 1))
+      print('Speak!')
+      recording = sd.rec(
+        int(duration * fs),
+        samplerate=fs,
+        channels=2,
+        blocking=True
+      )
+      print('Record {} done'.format(i + 1))
+      file_name = VoiceRecorder.SavePath + 'command_{}_record_{}.npy'.format(command, i)
+      np.save(file_name, recording)
