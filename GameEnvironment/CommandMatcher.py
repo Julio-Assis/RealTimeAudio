310fe3fd32044cb607dd0fa3e2330d0584c729bb
Julio Assis <juliosevarollia@gmail.com>
2019-06-14 14:25:09 +0200

    implemented CommandMatcher

---
+++ GameEnvironment/CommandMatcher.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+class CommandMatcher:
+
+  def __init__(self, command_files):
+    self.command_files = command_files
+    self.fft_transforms_by_command = {}
+    self.load_commands()
+
+  def load_commands(self):
+
+    for command_file in self.command_files:
+      command_fft = np.load(command_file)
+      file_name = command_file.split('/')[-1] # get only the last part of the path
+      command = file_name.split('.')[0] # remove the file extension
+      self.fft_transforms_by_command[command] = command_fft 
+
+  def get_closest(self, target_fft):
+
+    min_dist = np.inf
+    for command in self.fft_transforms_by_command.keys():
+        diff = target_fft - self.fft_transforms_by_command[command]
+        dist = np.linalg.norm(diff)
+
+        if dist < min_dist:
+          min_dist = dist
+          closest_command = command
+
+    return closest_command
+
+  def ranked_distances(self, target_fft):
+    distances = []
+    for command in self.fft_transforms_by_command:
+      diff = target_fft - self.fft_transforms_by_command[command]
+      dist = np.linalg.norm(diff)
+
+      distances.append([command, dist])
+
+    return sorted(distances, key=lambda x: x[1])
