2f6e887a0f4a2cea6aeea853105202c5f2982132
Julio Assis <juliosevarollia@gmail.com>
2019-06-18 20:33:08 +0200

    changed tabs to 4 spaces

--- GameEnvironment/CommandMatcher.py
+++ GameEnvironment/CommandMatcher.py
@@ -1,41 +1,43 @@
 import numpy as np
 
+
 class CommandMatcher:
 
-  def __init__(self, command_files):
-    self.command_files = command_files
-    self.fft_transforms_by_command = {}
-    self.load_commands()
+    def __init__(self, command_files):
+        self.command_files = command_files
+        self.fft_transforms_by_command = {}
+        self.load_commands()
 
-  def load_commands(self):
+    def load_commands(self):
 
-    for command_file in self.command_files:
-      command_fft = np.load(command_file)
-      file_name = command_file.split('/')[-1] # get only the last part of the path
-      command = file_name.split('.')[0] # remove the file extension
-      self.fft_transforms_by_command[command] = command_fft 
+        for command_file in self.command_files:
+            command_fft = np.load(command_file)
+            # get only the last part of the path
+            file_name = command_file.split('/')[-1]
+            command = file_name.split('.')[0]  # remove the file extension
+            self.fft_transforms_by_command[command] = command_fft
 
-  def get_closest(self, target_signal):
+    def get_closest(self, target_signal):
 
-    target_fft = np.fft.fft(target_signal)
-    min_dist = np.inf
-    for command in self.fft_transforms_by_command.keys():
-        diff = target_fft - self.fft_transforms_by_command[command]
-        dist = np.linalg.norm(diff)
+        target_fft = np.fft.fft(target_signal)
+        min_dist = np.inf
+        for command in self.fft_transforms_by_command.keys():
+            diff = target_fft - self.fft_transforms_by_command[command]
+            dist = np.linalg.norm(diff)
 
-        if dist < min_dist:
-          min_dist = dist
-          closest_command = command
+            if dist < min_dist:
+                min_dist = dist
+                closest_command = command
 
-    return closest_command
+        return closest_command
 
-  def ranked_distances(self, target_signal):
-    distances = []
-    target_fft = np.fft.fft(target_signal)
-    for command in self.fft_transforms_by_command:
-      diff = target_fft - self.fft_transforms_by_command[command]
-      dist = np.linalg.norm(diff)
+    def ranked_distances(self, target_signal):
+        distances = []
+        target_fft = np.fft.fft(target_signal)
+        for command in self.fft_transforms_by_command:
+            diff = target_fft - self.fft_transforms_by_command[command]
+            dist = np.linalg.norm(diff)
 
-      distances.append([command, dist])
+            distances.append([command, dist])
 
-    return sorted(distances, key=lambda x: x[1])
+        return sorted(distances, key=lambda x: x[1])
