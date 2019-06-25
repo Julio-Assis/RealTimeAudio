6d561747c5b12b408859e71172f1c0037c5e6e66
Julio Assis <juliosevarollia@gmail.com>
2019-06-25 22:15:59 +0200

    changed sample rate to 16k and frame length to 1s

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from GameEnvironment.VoiceRecorder import VoiceRecorder
+from GameEnvironment.DanielMatcher import DanielMatcher
 from GameEnvironment.FeatureExtractor import FeatureExtractor
 from game_menu import get_inputs_and_play
 
@@ -11,8 +12,8 @@ if __name__ == '__main__':
     # python keyboard_agent.py SpaceInvadersNoFrameskip-v4
     #
     game_name = 'MsPacman-v0' if len(sys.argv) < 2 else sys.argv[1]
-    frames = 2 * 44100
-    sample_rate = 44100
+    sample_rate = 16000
+    frames = 1 * sample_rate
     channels = 1
     voice_recorder = VoiceRecorder(game_name)
     voice_recorder.record_commands(frames, sample_rate, channels)
